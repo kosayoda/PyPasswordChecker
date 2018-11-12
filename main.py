b295f999f885c112829235e7402864778723755d
kosayoda <kieransiek@gmail.com>
2018-11-12 11:26:05 +0800

    Removed usage of global variables, fixed formatting

--- main.py
+++ main.py
@@ -11,19 +11,21 @@ upperRegex = re.compile(r"^(?=.*?[A-Z])")
 lowerRegex = re.compile(r"^(?=.*?[a-z])")
 onedigitRegex = re.compile(r"[0-9]+")
 
-password_length = 3
-
 
 class MainApplication(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
+
         self.master = master
         master.title("Password Checker")
         master.geometry("700x300")
         master.resizable(width=False, height=False)
         master.tk.call('wm', 'iconphoto', master._w, tk.PhotoImage(file='logo.png'))
+
         self._frame = None
         self.switch_frame(MenuPage)
+
+        self.password_length = 3
 
     def switch_frame(self, frame_class):
         new_frame = frame_class(self)
@@ -36,48 +38,56 @@ class MainApplication(tk.Frame):
 class MenuPage(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
-
-        start_label = tk.Label(self, text="Password Checker\n", font=(None, 24))
+        start_label = tk.Label(
+            self, text="Password Checker\n", font=(None, 24)
+        )
         start_label.grid(row=0, column=0, sticky=tk.NSEW)
+
         start_button = tk.Button(
-            self, text="Check Password", command=lambda: master.switch_frame(MainPage)
+            self, text="Check Password",
+            command=lambda: master.switch_frame(MainPage)
         )
         start_button.grid(row=1, column=0, sticky=tk.NSEW)
+
         credits_button = tk.Button(
-            self, text="Configure Parameters", command=lambda: master.switch_frame(SettingsPage)
+            self, text="Configure Parameters",
+            command=lambda: master.switch_frame(SettingsPage)
         )
         credits_button.grid(row=2, column=0, sticky=tk.NSEW)
-        quit_button = tk.Button(self, text="Quit", command=lambda: sys.exit())
-        quit_button.grid(row=3, column=0, sticky=tk.NSEW)
 
-        self.grid_columnconfigure(0, weight=1)
-        self.grid_rowconfigure(0, weight=1)
-        self.grid_rowconfigure(1, weight=1)
-        self.grid_rowconfigure(2, weight=1)
+        quit_button = tk.Button(
+            self, text="Quit",
+            command=lambda: sys.exit())
+
+        quit_button.grid(row=3, column=0, sticky=tk.NSEW)
 
 
 class SettingsPage(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
+
         frame_1 = tk.Frame(self)
-        tk.Label(frame_1, text="\nLength of Password:", font=("", 12)).pack(side=tk.LEFT, fill=tk.BOTH)
-        length_scale = tk.Scale(frame_1, from_=3, to=30, orient=tk.HORIZONTAL)
-        length_scale.pack()
-        self.grid_rowconfigure(0, weight=1)
-        self.grid_rowconfigure(1, weight=1)
+        passwordlen_label = tk.Label(frame_1, text="\nLength of Password:", font=("", 12))
+        passwordlen_label.pack(side=tk.LEFT, fill=tk.BOTH)
+
+        passwordlen_scale = tk.Scale(frame_1, from_=3, to=30, orient=tk.HORIZONTAL)
+        passwordlen_scale.pack()
         frame_1.grid(pady=10)
+
         end_frame = tk.Frame(self)
-        tk.Button(
-            end_frame, text="Save", command=lambda: self.save_settings(master, length_scale.get())
-        ).pack(side=tk.LEFT)
-        tk.Button(
+        save_button = tk.Button(
+            end_frame, text="Save", command=lambda: self.save_settings(master, passwordlen_scale.get())
+        )
+        save_button.pack(side=tk.LEFT)
+
+        back_button = tk.Button(
             end_frame, text="Back", command=lambda: master.switch_frame(MenuPage)
-        ).pack(side=tk.RIGHT)
+        )
+        back_button.pack(side=tk.RIGHT)
         end_frame.grid()
 
     def save_settings(self, master, length):
-        global password_length
-        password_length = length
+        master.password_length = length
         master.switch_frame(MenuPage)
 
 
@@ -91,7 +101,9 @@ class MainPage(tk.Frame):
         )
         self.image1.grid(column=0, row=0, padx=10, pady=10)
 
-        text1 = tk.Label(self, text="Password contains more than {} characters".format(password_length))
+        text1 = tk.Label(
+            self, text="Password contains more than {} characters".format(master.password_length)
+        )
         text1.grid(column=1, row=0, sticky=tk.W)
 
         self.image2 = tk.Label(
@@ -118,14 +130,20 @@ class MainPage(tk.Frame):
         self.entry1 = tk.Entry(self, width=48)
         self.entry1.grid(column=1, row=3, sticky=tk.W)
 
-        button1 = tk.Button(self, text="Submit", command=lambda: self.checkPass(self.entry1.get()))
+        button1 = tk.Button(
+            self, text="Submit",
+            command=lambda: self.checkPass(master, self.entry1.get())
+        )
         button1.grid(column=2, row=3, sticky=tk.W)
 
-        button2 = tk.Button(self, text="Back", command=lambda: master.switch_frame(MenuPage))
+        button2 = tk.Button(
+            self, text="Back",
+            command=lambda: master.switch_frame(MenuPage)
+        )
         button2.grid(column=0, row=4, pady=10)
 
-    def checkPass(self, text):
-        if len(text) > password_length:
+    def checkPass(self, master, text):
+        if len(text) > master.password_length:
             self.image1.configure(bg="green")
         else:
             self.image1.configure(bg="red")
